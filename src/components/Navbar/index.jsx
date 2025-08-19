fef6a2ad7b3106faf61e4503a385d8890e368da0
ARUN-KUMAR-YARLAGADDA <arunkumar_yarlagadda@srmap.edu.in>
2025-08-19 23:38:35 +0530

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+    return (
+        <>
+            <header className="block ">
+                <img src="/src/assets/download.png" alt="Logo" />
+                <h1>MyNotes</h1>
+            </header>
+        </>
+    )
+}
